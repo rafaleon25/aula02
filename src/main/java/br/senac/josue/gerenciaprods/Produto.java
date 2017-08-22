fb15c8dd34507f6ae8e7044c80f62ef908346155
rafael.rsilva22 <rafael.rsilva22@CAS511780W0726.senacsp.edu.br>
2017-08-22 20:09:32 -0300

    Hello world, projeto java inicial

---
+++ src/main/java/br/senac/josue/gerenciaprods/Produto.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.josue.gerenciaprods;
+
+/**
+ *
+ * @author rafael.rsilva22
+ */
+public class Produto {
+    private int idProd;
+    private String descricao, nome, categoria;
+    private double valorCompra, valorVenda;
+
+    public int getIdProd() {
+        return idProd;
+    }
+
+    public void setIdProd(int idProd) {
+        this.idProd = idProd;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getValorCompra() {
+        return valorCompra;
+    }
+
+    public void setValorCompra(double valorCompra) {
+        this.valorCompra = valorCompra;
+    }
+
+    public double getValorVenda() {
+        return valorVenda;
+    }
+
+    public void setValorVenda(double valorVenda) {
+        this.valorVenda = valorVenda;
+    }
+}
---
+++ src/main/java/br/senac/josue/gerenciaprods/ValidadorProd.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.josue.gerenciaprods;
+
+/**
+ *
+ * @author rafael.rsilva22
+ */
+public class ValidadorProd {
+    public static void validar (Produto prod) throws ProdutoException{
+         
+        if (prod == null) {
+            throw new ProdutoException("Não foi informado um produto.");
+        }
+        if(prod.getDescricao() == null || prod.getDescricao().length()>25){
+            throw new ProdutoException("Não foi informado uma descrição válida para o produto.");
+        }
+        if(prod.getNome()== null || prod.getNome().length()>50){
+            throw new ProdutoException("Não foi informado uma nome válida para o produto.");
+        }
+        if(prod.getCategoria() == null || prod.getCategoria().length()>30){
+            throw new ProdutoException("Não foi informado uma categoria válida para o produto.");
+        }
+        if(prod.getValorCompra() == 0d || prod.getValorCompra() < 0d){
+            throw new ProdutoException("Não foi informado um valor de compra válido.");
+        }
+        if(prod.getValorVenda()== 0d || prod.getValorVenda()< 0d){
+            throw new ProdutoException("Não foi informado um valor de compra válido.");
+        }
+    }
+}
